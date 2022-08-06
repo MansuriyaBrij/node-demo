fe7649589cb3fb7d4bfaec8df386568b62696149
MansuriyaBrij <77968876+MansuriyaBrij@users.noreply.github.com>
2022-08-06 17:11:57 +0530

    Initial commit

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+router.get("/1", function (req, res, next) {
+  res.send("111111");
+});
+
+module.exports = router;
